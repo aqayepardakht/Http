d96e295a09c48abc6b77d94f2c157af3d310e026
AqayePardakht <aqayepardakht@gmail.com>
2022-07-09 12:48:19 +0430

    fixed curl close bug

--- src/Response.php
+++ src/Response.php
@@ -12,9 +12,9 @@ class Response {
         $this->result     = curl_exec($ch);
         $this->statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-        curl_close($ch);
-
         if (curl_errno($ch)) $this->message = curl_error($ch);
+
+        curl_close($ch);
     }
 
     public function body() {
